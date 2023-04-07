0954895a0ad3e56b2e39038567da1ed4c906c0fb
jinnatul <morolswediu@gmail.com>
2023-04-07 14:53:28 +0600

    fix: jwt expire in

--- src/utils/createJWT.js
+++ src/utils/createJWT.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const createJWT = (data) => {
   const token = jwt.sign(data, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: data.expiresIn ? data.expiresIn : process.env.JWT_EXPIRES_IN,
   });
   return token;
 };
